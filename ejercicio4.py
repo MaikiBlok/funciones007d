ac5364af39f3c91795f2726df06cdb99268183a7
MaikiBlok <go.pizarrol@duocuc.cl>
2026-06-11 17:25:30 -0400

    Funcion con retorno y parametros

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,6 +1,6 @@
 def conversion_notas(puntaje,puntaje_total):
     nota= (puntaje * 6 / puntaje_total) +1
-    return nota
+    return round(nota,1)
 while True:
     try:
         p = float(input("Ingrese el puntaje obtenido:"))
@@ -18,6 +18,5 @@ while True:
         else:
             break
     except ValueError:
-        print("Favor ingresar un valor.")
-total=conversion_notas(p,pt)
+        print("Favor ingresar un valor númerico.")
 print(f"El estudiante obtuvo una nota de {conversion_notas(p,pt)}")
