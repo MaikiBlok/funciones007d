3d23093112e1a17fbc863cd168759cb72b0815c9
MaikiBlok <go.pizarrol@duocuc.cl>
2026-06-11 17:24:15 -0400

    Create ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,23 @@
+def conversion_notas(puntaje,puntaje_total):
+    nota= (puntaje * 6 / puntaje_total) +1
+    return nota
+while True:
+    try:
+        p = float(input("Ingrese el puntaje obtenido:"))
+        if p<0:
+            print("El valor no es válido")
+        else:
+            break
+    except ValueError:
+        print("Favor ingresar un valor.")
+while True:
+    try:
+        pt = float(input("Ingrese el puntaje máximo:"))
+        if p<0:
+            print("El valor no es válido")
+        else:
+            break
+    except ValueError:
+        print("Favor ingresar un valor.")
+total=conversion_notas(p,pt)
+print(f"El estudiante obtuvo una nota de {conversion_notas(p,pt)}")
